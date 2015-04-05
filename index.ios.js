775a8cb60d8f8a9eda60a054a477fcb741d7483e
Erik Nilsen <enilsen16@live.com>
2015-04-05 15:22:04 -0700

    multiple results

--- index.ios.js
+++ index.ios.js
@@ -8,6 +8,7 @@ var React = require('react-native');
 var {
   AppRegistry,
   Image,
+  ListView,
   StyleSheet,
   Text,
   View,
@@ -16,7 +17,10 @@ var {
 var rnexample = React.createClass({
   getInitialState: function () {
     return {
-      movies: null,
+      dataSource: new ListView.DataSource({
+        rowHasChanged: (row1, row2) => row1 !== row2,
+      }),
+      loaded: false,
     };
   },
   componentDidMount: function() {
@@ -27,18 +31,24 @@ var rnexample = React.createClass({
       .then((response) => response.json())
       .then((responseData) => {
         this.setState({
-          movies: responseData.movies,
+          dataSource: this.state.dataSource.cloneWithRows(responseData.movies),
+          loaded:true,
         });
       })
       .done();
   },
   render: function() {
-    if (!this.state.movies) {
+    if (!this.state.loaded) {
       return this.renderLoadingView();
     }
 
-    var movie = this.state.movies[0];
-    return this.renderMovie(movie);
+    return(
+      <ListView
+        dataSource={this.state.dataSource}
+        renderRow={this.renderMovie}
+        style={styles.listView}
+        />
+    );
   },
 
   renderLoadingView: function() {
@@ -85,6 +95,10 @@ var styles = StyleSheet.create({
   },
   year: {
     textAlign: 'center',
+  },
+  listView: {
+    paddingTop: 20,
+    backgroundColor: '#F5FCFF',
   },
   thumbnail: {
     width: 53,
